8d160f010bc23291ccce55acd30684f5b9796e95
Zee Mudia <iosazee1@gmail.com>
2023-04-26 13:17:34 +0100

    code for product, productcard and productdetail components added

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,39 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
+
+
+const ProductCard = ({product}) => {
+
+
+  return (
+    <Card sx={{boxShadow:'black', border: '1px solid #ccc'}}>
+      <LazyLoad height={500} offset={200} >
+        <CardMedia
+          sx={{ height: 500 }}
+          image={product.image}
+          title={product.title}
+        />
+      </LazyLoad>
+      <CardContent>
+        <Typography align="center" component="p" sx={{ minHeight: 70 }}>
+            {product.title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Link to={`/products/${product.id}`}  style={{width:"100%", textDecoration:"none"}} >
+          <Button fullWidth={true} variant='outlined' size='medium' sx={{backgroundColor: "#D1D1D1", color:"black", border: 'black solid 1px'}} color='error'>
+          £{product.price} - Details
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default ProductCard
---
+++ frontend/src/components/ProductDetail.js
@@ -0,0 +1,124 @@
+import React, {useState, useEffect} from 'react';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import { useParams, Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
+import axios from "axios";
+import { API_URL, fetchCartItems} from '../constants';
+import eventEmitter from '../constants/events';
+
+
+const ProductDetail = ({setCartItems}) => {
+
+    const [selectedProduct, setSelectedProduct] = useState(null)
+    let {id} = useParams()
+   
+
+    useEffect(() => {
+        axios.get(`${API_URL}products/${id}`)
+          .then(response => {
+            setSelectedProduct(response.data);
+            // console.log(response.data)
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }, [id]);
+
+
+
+      const handleAddItemToCart = () => {
+        const cartId = localStorage.getItem('cart_id');
+        const userId = localStorage.getItem('user_id');
+        const newItem = {
+          "product_id": selectedProduct.id,
+          "quantity": 1,
+          "user_id": userId
+        };
+        axios.post(`${API_URL}cart/${cartId}/items/`, newItem)
+          .then(response => {
+            // console.log( "PD Resp", response.data);
+            localStorage.setItem('cart_updated', true);
+            fetchCartItems(cartId, setCartItems);
+            // Emit the cartUpdated event with the updated cartCount
+            eventEmitter.emit('cartUpdated', response.data.total_quantity);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      };
+
+
+    const getRatingStars = (rating) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= Math.floor(rating)) {
+                stars.push(<StarIcon key={i} style={{ color: "yellow" }} />);
+            } else {
+                stars.push(<StarBorderIcon key={i} style={{ color: "yellow" }} />);
+            }
+        }
+        return stars;
+    };
+
+
+    return (
+        <Container sx={{flexGrow:1, display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <Card elevation={0} sx={{background: "transparent", my:3}}>
+                {
+                    selectedProduct ? (
+                        <Card sx={{padding: "20px 5px" }} >
+                            <Grid container> 
+                                <Grid item xs={12} sm={6}>
+                                    <LazyLoad height={350} offset={70} >
+                                        <CardMedia
+                                            sx={{ height: "100%", objectFit: "contain" }}
+                                            component="img"
+                                            image={selectedProduct.image}
+                                            title={selectedProduct.title}
+                                        />
+                                    </LazyLoad>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {selectedProduct.title}
+                                        </Typography>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {selectedProduct.price}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {selectedProduct.description}
+                                        </Typography>
+                                        <Typography variant="body2" style={{backgroundColor:"#394150", color:"#fff", borderRadius: "8px"}} component="div" my={2} > <Typography>Rating:</Typography>
+                                            {selectedProduct && getRatingStars(selectedProduct.rating)}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions sx={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
+                                        <Button variant="contained" size="small" onClick={handleAddItemToCart} data-testid="add-to-cart" color='inherit' sx={{mr:12 }} >Add to Cart</Button>
+                                        <Link to="/cart" style={{ textDecoration: "none", color: 'inherit' }} ><Button variant="contained" size="small" color='inherit'>View Cart</Button></Link>
+                                    </CardActions>
+                                </Grid>
+                            </Grid>
+                        </Card>
+                    ) : (
+                        <Typography>Loading...</Typography>
+                    )
+                }
+                <Link to="/" style={{ textDecoration: "none", color:'black'}} >
+                    <Button variant="contained" size="medium" color='error' sx={{ mt: 3 }}>Go back</Button>
+                </Link>
+            </Card>
+        </Container>
+    );
+}
+
+export default ProductDetail;

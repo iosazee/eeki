1abd1f251973ead763e2d050a594ba1737c4dd87
Zee Mudia <iosazee1@gmail.com>
2023-04-26 13:25:42 +0100

    code for payment confirmation added

---
+++ frontend/src/components/PaymentConfirmation.js
@@ -0,0 +1,30 @@
+import { Card, Typography } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+
+const PaymentConfirmation = () => {
+  const navigate = useNavigate();
+  const { orderId } = useParams();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 3000); // Redirect the user to the homepage after 3 seconds
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <section style={{ marginBottom: 25 }}>
+      <Card style={{ maxWidth: 650, margin: "0 auto", padding: "20px 5px" }}>
+        <Typography variant="h3" style={{ marginTop: 25 }}>
+          Thank you for your purchase!
+        </Typography>
+        <Typography variant="h3" style={{ marginTop: 25 }}>
+          Your items are on the way
+        </Typography>
+      </Card>
+    </section>
+  );
+};
+
+export default PaymentConfirmation;
